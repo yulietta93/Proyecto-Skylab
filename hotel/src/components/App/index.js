648e72835d7505e6b080d062a72e8cae73ea645c
Yulietta <yuliettadelaudi@gmail.com>
2019-04-25 22:25:34 +0200

    maquetacion-cabecera

--- hotel/src/components/App/index.js
+++ hotel/src/components/App/index.js
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
-import HomeBackground from 'HomeBackground.jpg';
+import HomeBackground from "./HomeBackground.jpg";
+import logo from "./logo-blanco.svg";
+import "./index.css";
 
 class App extends Component {
   render() {
     return (
       <div class="container-home">
-        <h1>
-          Welcome to <br />your news home
-        </h1>
-        <div className="cabecera">
-          <img src={HomeBackground} alt="logotipo" width="100%" />
+        <img
+          src={HomeBackground}
+          alt="background"
+          width="100%"
+          className="background-home"
+        />
+        <img src={logo} alt="logotipo" width="100%" className="logo" />
+
+        <div className="texto-cabecera">
+          <h1>
+            Welcome to <br />
+            your news home
+          </h1>
+          <span className="slogan-h1">Slogan, slogan slogan ipsum</span>
+          <span className="discover">Discover nature</span>
+          <span className="scroll"> Scroll more</span>
+          <span className="slogan-vertical"> Slogan, slogan slogan ipsum</span>
         </div>
+        <div class ="container-body">
+        <div className="about">
+          <h2>
+            What's <br />
+            NN?
+          </h2>
+          <p className="text-about">
+            Ark-Shelters are prefabricated, fully finished, modular <br/>units. By a
+            lake, deep in a forest, at the edge of a field,<br/> a steep mountain
+            hill or your<br/>city rooftop! Make your dream come true, installed in
+            one day,<br/> no extra on-site work.
+          </p>
+        </div>
+      </div>
       </div>
     );
   }
