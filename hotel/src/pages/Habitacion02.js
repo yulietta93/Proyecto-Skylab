9c41eafcd5bfe045f3610f4510a9efb7ac12213a
Yulietta <yuliettadelaudi@gmail.com>
2019-05-01 20:25:29 +0200

    service page contenido

--- hotel/src/pages/Habitacion02.js
+++ hotel/src/pages/Habitacion02.js
@@ -1,14 +1,79 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/Footer";
+import PictosRoom from "../components/Pictos/PictosRoom";
+import piscina from "../image/piscina.jpg";
+
+
+//FALTA AÑADIR SLIDER GALERIA
+
+const RoomDetail = props => (
+  <div className="rooms-container">
+    <div className="image-rooms-home">
+      <img className="image-room-principal" src={props.imageroom} width="20%" />
+      <img className="image-room-sec" src={props.imageroom2} width="20%" />
+      <img className="image-room-terc" src={props.imageroom3} width="20%" />
+    </div>
+    <p>{props.descriptionroom} </p>
+  </div>
+);
 
 export default class Habitacion02 extends Component {
-    render() {
-      return (
-        <div>
-          Habitacion02
-          <Link to="/habitaciones/:habitacion02">Habitacion 02</Link>
-        </div>
-      )
-    }
+  constructor() {
+    super();
+    this.state = {
+      list: [
+        {
+          imageroom:
+            "https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+          imageroom2:
+            "https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+          imageroom3:
+            "https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+          descriptionroom:
+            "With all the amenities you need.For you, your family and your friends"
+        }
+      ]
+    };
   }
-  
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.state.list.map(element => (
+          <RoomDetail
+            imageroom={element.imageroom}
+            imageroom2={element.imageroom2}
+            imageroom3={element.imageroom3}
+            descriptionroom={element.descriptionroom}
+          />
+        ))}
+        <PictosRoom />
+        <div className="destacado">
+          <h4>
+            Destacado-Reward yourself with a <br /> hot shower after a long day
+            and
+            <br /> then slide in-between the sheets
+            <br /> of our incredible beds.
+          </h4>
+          <p className="text-destacado">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit placerat
+            nullam, netus elementum phasellus cursus sagittis mollis arcu.
+            Ultrices habitasse felis at maecenas orci risus turpis, curae
+            pharetra nostra purus tortor pretium justo sodales,
+          </p>
+          <img
+            src={piscina}
+            alt="img-destacado-room"
+            className="img-destacado-room"
+            width="20%"
+          />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
--- hotel/src/pages/Servicios.js
+++ hotel/src/pages/Servicios.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./home.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
-export default class Habitacion01 extends Component {
-    render() {
-      return (
-        <div>
-          servicios
-          <Link to="/servicios"> Servicios</Link>
+//image
+import piscina from "../image/piscina.jpg";
+
+//componentes
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/Footer";
+import PictosHome from "../components/Pictos/PictosHome";
+
+export default class Servicios extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        
+        <div className="section01">
+            <h2> Discover the real <br /> ipsum nature!</h2>
+            <p className="text-service">
+               Lorem ipsum dolor sit amet consectetur adipiscing elit nibh
+               venenatis, tellus malesuada tempor curabitur iaculis ultricies nulla
+               lectus rutrum porttitor. Integer pellentesque nascetur turpis semper
+               pulvinar nisl sodales vivamus ligula, luctus vulputate tristique
+               torquent curae nulla placerat mi curabitur suscipit, nostra cras
+               euismod at.</p>
+          <img src={piscina} alt="img-destacado-room" className="img-destacado-room"width="20%"/>
+          <button onClick="">Boow now</button>
         </div>
-      )
-    }
+
+        <div className="section02">
+            <h2>Find your room in a <br/>small oasis in the <br/>middle of nature</h2>
+            <img src={piscina} alt="img-destacado-room" className="img-destacado-room"width="20%"/>
+            <img src={piscina} alt="img-destacado-room" className="img-destacado-room"width="20%"/>
+            <p className="text-service02">Lorem ipsum dolor sit amet consectetur adipiscing elit nibh venenatis </p>
+            <button onClick="">Boow now</button>
+        </div>
+
+        <PictosHome />
+
+        <div className="section03">
+          <h2>Reward yourself with a hot <br/>shower after a long day and then<br/>slide in-between the sheets of our <br/>incredible beds.</h2>
+          <img src={piscina} alt="img-destacado-room" className="img-destacado-room" width="20%"/>
+          <button onClick="">Boow now</button>
+        </div>
+
+        <Footer />
+      </div>
+    );
   }
-  
+}
